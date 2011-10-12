1a33976cd043c0f6227ee43f9b54abea7793d37c
Stan Seibert <stan@mtrr.org>
2011-10-11 20:29:00 -0400

    Add a few more commonly used symbols to: from chroma import *

--- chroma/__init__.py
+++ chroma/__init__.py
@@ -3,9 +3,11 @@ from chroma import geometry
 from chroma import event
 from chroma.io import root
 from chroma import generator
+from chroma.generator import constant_particle_gun
 from chroma import gpu
 from chroma import itertoolset
 from chroma import likelihood
+from chroma.likelihood import Likelihood
 from chroma import make
 from chroma.demo import optics
 from chroma import project
