2e55224b48951b6221bea1921f015684eea96325
Benjamin Land <benland100@gmail.com>
2019-11-07 16:34:38 -0500

    Swap in slightly different triangle intersection code.
    Behaves better at triangle boundaries - numerical instabilities present
    in old calculation make edges behave strangely.

--- chroma/cuda/intersect.h
+++ chroma/cuda/intersect.h
@@ -6,17 +6,27 @@
 #include "linalg.h"
 #include "matrix.h"
 #include "geometry.h"
+#include <float.h>
 
-#define EPSILON 0.0f
+
+// INFINITY is already defined elsewhere
+#define CHROMA_INFINITY __int_as_float(0x7f800000)
+#define CHROMA_EPSILON 1e-6
 
 /* Tests the intersection between a ray and a triangle.
    If the ray intersects the triangle, set `distance` to the distance from
    `origin` to the intersection and return true, else return false.
-   `direction` must be normalized to one. */
+   `direction` must be normalized to one. 
+   
+   old impl commented out, new stolen from https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+   likely same algorithm, need to check.
+   
+   */
 __device__ bool
 intersect_triangle(const float3 &origin, const float3 &direction,
 		   const Triangle &triangle, float &distance)
 {
+	/*
 	float3 m1 = triangle.v1-triangle.v0;
 	float3 m2 = triangle.v2-triangle.v0;
 	float3 m3 = -direction;
@@ -34,14 +44,14 @@ intersect_triangle(const float3 &origin, const float3 &direction,
 		    (m.a02*m.a21 - m.a01*m.a22)*b.y +
 		    (m.a01*m.a12 - m.a02*m.a11)*b.z)/determinant;
 
-	if (u1 < -EPSILON || u1 > 1.0f)
+	if (u1 < 0.0f || u1 > 1.0f)
 		return false;
 
 	float u2 = ((m.a12*m.a20 - m.a10*m.a22)*b.x +
 		    (m.a00*m.a22 - m.a02*m.a20)*b.y +
 		    (m.a02*m.a10 - m.a00*m.a12)*b.z)/determinant;
 
-	if (u2 < -EPSILON || u2 > 1.0f)
+	if (u2 < 0.0f || u2 > 1.0f)
 		return false;
 
 	float u3 = ((m.a10*m.a21 - m.a11*m.a20)*b.x +
@@ -54,6 +64,34 @@ intersect_triangle(const float3 &origin, const float3 &direction,
 	distance = u3;
 
 	return true;
+	*/
+        float3 edge1, edge2, h, s, q;
+        float a,f,u,v;
+        edge1 = triangle.v1 - triangle.v0;
+        edge2 = triangle.v2 - triangle.v0;
+        h = cross(direction,edge2);
+        a = dot(edge1,h);
+        if (a > -FLT_EPSILON && a < FLT_EPSILON)
+            return false;    // This ray is parallel to this triangle.
+        f = 1.0/a;
+        s = origin - triangle.v0;
+        u = f * dot(s,h);
+        if (u < -CHROMA_EPSILON || u > 1.0+CHROMA_EPSILON)
+            return false;
+        q = cross(s,edge1);
+        v = f * dot(direction,q);
+        if (v < -CHROMA_EPSILON || u + v > 1.0+CHROMA_EPSILON)
+            return false;
+        // At this stage we can compute t to find out where the intersection point is on the line.
+        float t = f * dot(edge2,q);
+        if (t > CHROMA_EPSILON && t < CHROMA_INFINITY) // ray intersection
+        {
+            //outIntersectionPoint = origin + direction * t;
+            distance = t;
+            return true;
+        }
+        else // This means that there is a line intersection but not a ray intersection.
+            return false;
 }
 
 /* Tests the intersection between a ray and an axis-aligned box defined by
@@ -64,8 +102,6 @@ intersect_triangle(const float3 &origin, const float3 &direction,
     Source: Optimizing ray tracing for CUDA by Hannu Saransaari
     https://wiki.aalto.fi/download/attachments/40023967/gpgpu.pdf
 */
-// INFINITY is already defined elsewhere
-#define CHROMA_INFINITY __int_as_float(0x7f800000)
 
 __device__ bool
 intersect_box(const float3 &neg_origin_inv_dir, const float3 &inv_dir,
