83339db033955a43eaf10cfa59e76473229ce028
Benjamin Land <benland100@gmail.com>
2019-11-15 18:27:53 -0500

    Maybe this Dockerfile results in a usable build!

--- installation/chroma_env.sh
+++ installation/chroma_env.sh
@@ -1,5 +1,11 @@
 if [ -e /opt/anaconda3/bin/conda ]; then
     eval "$(/opt/anaconda3/bin/conda shell.bash hook)"
+    export LD_LIBRARY_PATH="/opt/anaconda3/lib:$LD_LIBRARY_PATH"
+fi
+if [ -e /opt/boost ]; then
+    export LD_LIBRARY_PATH="/opt/boost/lib:$LD_LIBRARY_PATH"
+    export PATH="/opt/boost/bin:$PATH"
+    export BOOST_ROOT="/opt/boost"
 fi
 if [ -e /opt/root/bin/thisroot.sh ]; then
     source /opt/root/bin/thisroot.sh
