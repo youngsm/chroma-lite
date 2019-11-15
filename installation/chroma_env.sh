ab91a61e6b6ed92dbc507d0500660f13cab5c31b
Benjamin Land <benland100@gmail.com>
2019-11-15 11:14:25 -0500

    Adjust Dockerfile for better caching and linking issues.

--- installation/chroma_env.sh
+++ installation/chroma_env.sh
@@ -1,3 +1,10 @@
-eval "$(/opt/anaconda3/bin/conda shell.bash hook)"
-source /opt/root/bin/thisroot.sh
-source /opt/geant4/bin/geant4.sh
+if [ -e /opt/anaconda3/bin/conda ]; then
+    eval "$(/opt/anaconda3/bin/conda shell.bash hook)"
+fi
+if [ -e /opt/root/bin/thisroot.sh ]; then
+    source /opt/root/bin/thisroot.sh
+fi
+if [ -e /opt/geant4/bin/geant4.sh ]; then
+    source /opt/geant4/bin/geant4.sh
+    export PYTHONPATH="/opt/geant4/lib:$PYTHONPATH"
+fi
