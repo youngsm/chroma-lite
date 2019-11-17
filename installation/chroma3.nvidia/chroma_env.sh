472a5b49e62433d59004e712dee7c289e72a3065
Benjamin Land <benland100@gmail.com>
2019-11-16 20:14:23 -0500

    Try docker image derived from nvidia/cudagl

---
+++ installation/chroma3.nvidia/chroma_env.sh
@@ -0,0 +1,11 @@
+if [ -e /opt/anaconda3/bin/conda ]; then
+    eval "$(/opt/anaconda3/bin/conda shell.bash hook)"
+    export BOOST_ROOT=/opt/anaconda3
+fi
+if [ -e /opt/root/bin/thisroot.sh ]; then
+    source /opt/root/bin/thisroot.sh
+fi
+if [ -e /opt/geant4/bin/geant4.sh ]; then
+    source /opt/geant4/bin/geant4.sh
+    export PYTHONPATH="/opt/geant4/lib:$PYTHONPATH"
+fi
