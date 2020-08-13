1dbf1af64c1a1572aa240b66731b064b9137dc55
Benjamin Land <benland100@gmail.com>
2020-08-13 11:20:40 -0400

    Fix non-nvidia docker builds & update docker hub.

--- installation/rebuild.sh
+++ installation/rebuild.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-#docker_dirs="chroma3.deps chroma3.435 chroma3.440 chroma3.latest chroma3.nvidia.base chroma3.nvidia"
-docker_dirs="chroma3.nvidia.base chroma3.nvidia"
+docker_dirs="chroma3.deps chroma3.435 chroma3.440 chroma3.latest chroma3.nvidia.base chroma3.nvidia"
+#docker_dirs="chroma3.nvidia.base chroma3.nvidia"
 
 for dir in $docker_dirs; do
     image=$(echo $dir | sed 's/chroma3./benland100\/chroma3:/')
